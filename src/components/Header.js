ac6e91e3e11346e5cc07845aa6aeeeadd1d7f0bf
Ajay <lci2021023@iiitl.ac.in>
2022-10-02 09:13:42 +0530

    Added Dark Mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
+
 const Header = () => {
+
+  const [theme, setTheme] = useState("light-theme");
+  const [state,setState] = useState(false);
+
+  const toggleTheme = () => {
+    if (theme === "dark-theme") {
+      setState(!state);
+      setTheme('light-theme');
+    } else {
+      setTheme('dark-theme');
+      setState(!state);
+    }
+  }
+
+  useEffect(() => {
+    document.body.className = theme;
+  }, [theme])
+
   return (
+
+
     <div className="nav">
       <div className="ui fixed menu">
         <div className="ui container center">
           <h2>Sun-Geet</h2>&nbsp;by{" "}
           <a href="https://geethub.netlify.app">Geet-Hub</a>
           <div className="right menu">
+            <a href="#" className="btn" onClick={() => toggleTheme()}>{state ? 'Light Mode' : 'Dark Mode' }</a>
             <div className="item">
               <a
                 href="https://github.com/PrerakMathur20/geet-hub-community"
